f6cdfaff7f572fd5de2c1f4603bfe8a5a04ff92e
Dmitrii <directsdi@gmail.com>
2022-10-20 19:11:38 +0500

    Вывод количества четных чиле в случайном моссиве трехзначных чисел

---
+++ task_1_chetnye/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите длину массива:  ");
+try
+{
+int L = int.Parse (Console.ReadLine ());
+int [] massiv = new int [L];
+Random rand = new Random();
+for (int i = 0; i < L; i++)
+{
+   massiv [i] = rand.Next(100,1000);
+   System.Console.Write(massiv[i]+", ");
+}
+int count = 0;
+for (int i = 0; i < L; i++)
+{
+   if (massiv [i]%2==0)
+   count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Количество четных чисел в массиве  "+count);
+}
+catch
+{
+    System.Console.WriteLine("Вы ввели не число");
+}
